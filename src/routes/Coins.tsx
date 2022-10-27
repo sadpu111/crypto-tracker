e64c2db3d6729c163e2f09712749f967588d4809
sadpu111 <dwme7100@gmail.com>
2022-10-27 22:05:19 +0900

    gitignore 수정

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -33,7 +33,7 @@ const CoinsList = styled.ul``;
 
 const Coin = styled.li`
   background-color: white;
-  color: ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.textColor};
   margin-bottom: 10px;
   border-radius: 10px;
   a { // Coin 컴포넌트 내의 anchor는 아래와 같은 스타일 적용
